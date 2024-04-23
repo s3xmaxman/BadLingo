973317716d3378818fbdf835be5f23acb19aee31
s3xmaxman <badside3919@gmail.com>
2024-04-24 06:39:23 +0900

    add Learn Page Wrappers

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,15 +9,13 @@ type Props = {
 
 const MarketingLayout = ({ children }: Props) => {
     return (
-        <ClerkProvider>
             <div className="min-h-screen flex flex-col">
                 <Header />
-                <main className="flex-1 flex flex-col items-center justify-items-center">
-                    {children}
-                </main>
+                    <main className="flex-1 flex flex-col items-center justify-items-center">
+                        {children}
+                    </main>
                 <Footer />
             </div>
-        </ClerkProvider>
     )
 };
 
