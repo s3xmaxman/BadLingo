68a2f45ced5467f356aa203716942665f2b55e04
s3xmaxman <badside3919@gmail.com>
2024-04-25 03:01:58 +0900

    set up drizzle and Neon

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+
+export const courses = pgTable("courses", {
+    id: serial("id").notNull().primaryKey(),
+    title: text("title").notNull(),
+    imageSrc: text("image_src").notNull(),
+});
